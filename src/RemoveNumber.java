a7e3b4206e5df162ca4e1dd21871fb4d1b9793f9
AAstashenkov <arsenkaman@gmail.com>
2023-05-31 07:58:13 +0300

    homework5

--- src/RemoveNumber.java
+++ src/RemoveNumber.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class RemoveNumber {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 2, 4, 2, 5};
+        int[] array = {1, 2, 2, 3, 4, 2, 5};
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input number: ");
         int number = scanner.nextInt();
@@ -12,7 +12,6 @@ public class RemoveNumber {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == number) {
                 count++;
-                array[i] = 0;
             }
         }
 
@@ -20,14 +19,14 @@ public class RemoveNumber {
             int[] newArray = new int[array.length - count];
             int index = 0;
             for (int i = 0; i < array.length; i++) {
-                if (array[i] != 0) {
+                if (array[i] != number) {
                     newArray[index++] = array[i];
                 }
             }
             array = newArray;
-            System.out.println("Number was delete.");
+            System.out.println("Number was deleted.");
         } else {
-            System.out.println("Not found number.");
+            System.out.println("Number not found.");
         }
 
         System.out.println("Array: " + Arrays.toString(array));
